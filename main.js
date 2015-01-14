4c6e2b12ffc9dbb908a49fde838485adb1f5108c
Ian Smith-Heisters <i@idiosyncra.tc>
2015-01-13 17:33:43 -0800

    get infinite drops working with texture swapping
    add velocity-affected drop sizes

--- main.js
+++ main.js
@@ -79,15 +79,34 @@ _.extend( views.Base.prototype, {
 views.Paint = extend( views.Base, function( stage, w, h, timer ) {
   this.timer = timer;
 
-  this.gfx = new PIXI.Graphics();
-  stage.addChild( this.gfx );
+  this.mouseTrap = new PIXI.DisplayObject();
+  stage.addChild( this.mouseTrap );
 
-  this.gfx.hitArea = new PIXI.Rectangle( 0, 0, w, h );
-  this.gfx.interactive = true;
-  this.gfx.mousemove = this.gfx.touchmove = this.onMouseMove.bind( this );
-  this.gfx.mouseup = this.onMouseUp.bind( this );
+  this.mouseTrap.hitArea = new PIXI.Rectangle( 0, 0, w, h );
+  this.mouseTrap.interactive = true;
+  this.mouseTrap.mousemove = this.mouseTrap.touchmove = this.onMouseMove.bind( this );
+  this.mouseTrap.mouseup = this.onMouseUp.bind( this );
 
   this.pts = [];
+
+  this.gfx = new PIXI.Graphics();
+  // stage.addChild( this.gfx );
+
+  this.tex_in = new PIXI.RenderTexture( w, h );
+  this.tex_out = new PIXI.RenderTexture( w, h );
+  this.texSprite_in = new PIXI.Sprite( this.tex_in );
+  this.texSprite_out = new PIXI.Sprite( this.tex_out );
+
+  stage.addChild( this.texSprite_out );
+  this.offscreenContainer = new PIXI.DisplayObjectContainer();
+  this.offscreenContainer.addChild( this.texSprite_in );
+  this.offscreenContainer.addChild( this.gfx );
+
+  // flip out
+  this.texSprite_out.y = h;
+  this.texSprite_out.scale.y = -1;
+
+  this.texDirty = false;
 }, {
   update: function( time ) {
     var pt = this.pts[ 0 ];
@@ -105,15 +124,33 @@ views.Paint = extend( views.Base, function( stage, w, h, timer ) {
       vec2.add( avg, avg, vec2.scale( vec2.create(), delta, 1.0 / this.pts.length ) );
     }
 
-    var last = this.pts.length !== 0 ? this.pts[ this.pts.length - 1 ].v : vec2.create();
-    var lastPlusAvg = vec2.add( vec2.create(), last, avg );
 
-    this.gfx.clear();
-    this.gfx.beginFill( 0x000000, 1.0 );
-    this.gfx.lineStyle( 2, 0x000000, 1.0 );
-    this.gfx.moveTo( last[0], last[1] );
-    this.gfx.lineTo( lastPlusAvg[0], lastPlusAvg[1] );
-    this.gfx.endFill();
+    if ( this.pts.length ) {
+      var last = this.pts[ this.pts.length - 1 ].v;
+
+      this.gfx.clear();
+      this.gfx.beginFill( 0x000000, 1.0 );
+      for ( var i = 0; i < (Math.random() * 10); ++i ) {
+        var xy = vec2.add( vec2.create(), last, vec2.scale( vec2.create(), avg, Math.random() ) );
+        var r = Math.random() * vec2.length( avg ) * 0.5 + 3.0;
+        this.gfx.drawCircle( xy[0], xy[1], r );
+      }
+      this.gfx.endFill();
+
+      this.tex_out.render( this.offscreenContainer, undefined, true );
+      this.texDirty = true;
+    }
+  },
+
+  afterRender: function() {
+    if ( this.texDirty ) {
+      var temp = this.tex_in;
+      this.tex_in = this.tex_out;
+      this.tex_out = temp;
+      this.texSprite_in.setTexture( this.tex_in );
+      this.texSprite_out.setTexture( this.tex_out );
+      this.texDirty = false;
+    }
   },
 
   onMouseMove: function( ev ) {
@@ -131,7 +168,7 @@ views.Paint = extend( views.Base, function( stage, w, h, timer ) {
 var init = function( $container ) {
   var w = $container.width(), h = $container.height();
 
-  var stage = new PIXI.Stage( 0xFF0000, true /* interactive */ );
+  var stage = new PIXI.Stage( 0xFFFFFF, true /* interactive */ );
   var renderer = PIXI.autoDetectRenderer( w, h, {
     view: $container[0], antialias: true
   } );
@@ -154,6 +191,7 @@ var init = function( $container ) {
 
     view.update( timer.getElapsedSeconds() );
     renderer.render( stage );
+    view.afterRender();
   }
 };
 
